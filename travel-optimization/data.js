92e4cbe89912eaf5d560062c943ff67e170da570
Jozef Lacny <lacny.jozef@gmail.com>
2018-06-15 14:47:12 +0200

    travel-optimization demo

---
+++ travel-optimization/data.js
@@ -0,0 +1,301 @@
+
+var travelData = [
+    {
+    "id": "hotel:21292069",
+    "level": "poi",
+    "rating": 0.022853355893965,
+    "ratingLocal": 0.022853355893965,
+    "location": {
+    "lat": 51.49845,
+    "lng": -0.11343
+    },
+    "quadkey": "031313131130103301",
+    "name": "Park Plaza London Waterloo",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": "Offering a restaurant, Park Plaza London Waterloo is located in London, a short walk from Lambeth North underground station and less than a…",
+    "url": "https://travel.sygic.com/go/hotel:21292069",
+    "thumbnailUrl": "https://media-cdn.sygictraveldata.com/media/hotel:21292069",
+    "marker": "sleeping:hotel",
+    "categories": [
+    "eating",
+    "sleeping"
+    ],
+    "parentIds": [
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": 4,
+    "starRatingUnofficial": null,
+    "customerRating": 8.7,
+    "detail": null
+    },
+    {
+    "id": "poi:460",
+    "level": "poi",
+    "rating": 6.4572188621522,
+    "ratingLocal": 6.4572188621522,
+    "location": {
+    "lat": 51.5032859,
+    "lng": -0.1196861
+    },
+    "quadkey": "031313131130101220",
+    "name": "Coca‑Cola London Eye",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": "Come for a ride on Europe's largest Ferris wheel. The views from the top are simply fabulous, especially at night.",
+    "url": "https://travel.sygic.com/go/poi:460",
+    "thumbnailUrl": "https://media-cdn.sygictraveldata.com/media/poi:460",
+    "marker": "sightseeing:ferris_wheel",
+    "categories": [
+    "sightseeing",
+    "playing"
+    ],
+    "parentIds": [
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": null,
+    "starRatingUnofficial": null,
+    "customerRating": null,
+    "detail": null,
+    "spentTime": "01:00:00",
+    "preferredTimes": [
+    {
+    "start": "2018-06-08T18:00:00.000Z",
+    "end": "2018-06-08T20:00:00.000Z"
+    }
+    ]
+    },
+    {
+    "id": "poi:32281",
+    "level": "poi",
+    "rating": 2.0337785432319,
+    "ratingLocal": 2.0337785432319,
+    "location": {
+    "lat": 51.5042691,
+    "lng": -0.1347305
+    },
+    "quadkey": "031313131130011303",
+    "name": "The Mall",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": "This long and broad promenade lined by tall leafy trees takes you from Trafalgar Square straight to Buckingham Palace.",
+    "url": "https://travel.sygic.com/go/poi:32281",
+    "thumbnailUrl": "https://media-cdn.sygictraveldata.com/media/poi:32281",
+    "marker": "sightseeing",
+    "categories": [
+    "sightseeing"
+    ],
+    "parentIds": [
+    "city:87673",
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": null,
+    "starRatingUnofficial": null,
+    "customerRating": null,
+    "detail": null,
+    "spentTime": "01:00:00"
+    },
+    {
+    "id": "poi:453",
+    "level": "poi",
+    "rating": 6.9482180012398,
+    "ratingLocal": 6.9482180012398,
+    "location": {
+    "lat": 51.4993541,
+    "lng": -0.1274043
+    },
+    "quadkey": "031313131130102031",
+    "name": "Westminster Abbey",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": "First inhabited by Benedictine monks back in the 10th century, this Gothic church is one of the most important icons of London. ",
+    "url": "https://travel.sygic.com/go/poi:453",
+    "thumbnailUrl": "https://media-cdn.sygictraveldata.com/media/poi:453",
+    "marker": "other:place_of_worship:church",
+    "categories": [
+    "sightseeing",
+    "discovering"
+    ],
+    "parentIds": [
+    "city:87673",
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": null,
+    "starRatingUnofficial": null,
+    "customerRating": null,
+    "detail": null,
+    "spentTime": "00:30:00",
+    "openingHours": [
+    {
+    "start": "2018-06-08T09:00:00.000Z",
+    "end": "2018-06-08T10:00:00.000Z"
+    }
+    ]
+    },
+    {
+    "id": "poi:442",
+    "level": "poi",
+    "rating": 7.3679291479733,
+    "ratingLocal": 7.3679291479733,
+    "location": {
+    "lat": 51.5079883,
+    "lng": -0.1280252
+    },
+    "quadkey": "031313131130100010",
+    "name": "Trafalgar Square",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": "Designed by John Nash in 1830, Trafalgar Square is the largest square in London.",
+    "url": "https://travel.sygic.com/go/poi:442",
+    "thumbnailUrl": "https://media-cdn.sygictraveldata.com/media/poi:442",
+    "marker": "sightseeing:monument:national",
+    "categories": [
+    "sightseeing"
+    ],
+    "parentIds": [
+    "city:87673",
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": null,
+    "starRatingUnofficial": null,
+    "customerRating": null,
+    "detail": null
+    },
+    {
+    "id": "poi:441",
+    "level": "poi",
+    "rating": 9.4131489443436,
+    "ratingLocal": 9.4131489443436,
+    "location": {
+    "lat": 51.5006895,
+    "lng": -0.1245839
+    },
+    "quadkey": "031313131130102103",
+    "name": "Big Ben",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": "The most famous bell in Europe, perhaps even in the world, Big Ben, is one of the iconic landmarks of London.",
+    "url": "https://travel.sygic.com/go/poi:441",
+    "thumbnailUrl": "https://media-cdn.sygictraveldata.com/media/poi:441",
+    "marker": "other:building:tower:bell",
+    "categories": [
+    "sightseeing"
+    ],
+    "parentIds": [
+    "city:87673",
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": null,
+    "starRatingUnofficial": null,
+    "customerRating": null,
+    "detail": null,
+    "spentTime": "00:30:00"
+    },
+    {
+    "id": "poi:24560996",
+    "level": "poi",
+    "rating": 0.001,
+    "ratingLocal": 0.001,
+    "location": {
+    "lat": 51.5064092,
+    "lng": -0.1267743
+    },
+    "quadkey": "031313131130100031",
+    "name": "Grand Caffe Concerto",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": null,
+    "url": "https://travel.sygic.com/go/poi:24560996",
+    "thumbnailUrl": null,
+    "marker": "eating:restaurant",
+    "categories": [
+    "eating"
+    ],
+    "parentIds": [
+    "city:87673",
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": null,
+    "starRatingUnofficial": null,
+    "customerRating": null,
+    "detail": null,
+    "spentTime": "01:00:00",
+    "preferredTimes": [
+    {
+    "start": "2018-06-08T12:00:00.000Z",
+    "end": "2018-06-08T13:00:00.000Z"
+    }
+    ]
+    },
+    {
+    "id": "poi:443",
+    "level": "poi",
+    "rating": 2.9341899422499,
+    "ratingLocal": 2.9341899422499,
+    "location": {
+    "lat": 51.508861,
+    "lng": -0.1289608
+    },
+    "quadkey": "031313131112322232",
+    "name": "National Gallery",
+    "nameSuffix": "London, United Kingdom",
+    "boundingBox": null,
+    "perex": "Established in 1824, the art gallery holds an enormous art collection of over 2.600 pieces - no wonder it is one of the most visited…",
+    "url": "https://travel.sygic.com/go/poi:443",
+    "thumbnailUrl": "https://media-cdn.sygictraveldata.com/media/poi:443",
+    "marker": "discovering:gallery:national",
+    "categories": [
+    "discovering",
+    "sightseeing"
+    ],
+    "parentIds": [
+    "city:87673",
+    "region:2006111",
+    "region:228",
+    "city:1",
+    "region:44",
+    "country:1",
+    "continent:1"
+    ],
+    "starRating": null,
+    "starRatingUnofficial": null,
+    "customerRating": null,
+    "detail": null,
+    "spentTime": "02:00:00"
+    }
+    ]
